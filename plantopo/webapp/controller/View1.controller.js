9fe53b08a548b3f89c847dffd88ca84ea28b688f
Tom Jeong <jshipod98@gmail.com>
2025-05-26 00:57:02 +0900

    변경사항 내용

--- plantopo/webapp/controller/View1.controller.js
+++ plantopo/webapp/controller/View1.controller.js
@@ -10,6 +10,9 @@ sap.ui.define(
 
     return Controller.extend("sync.dc.pp.plantopo.controller.View1", {
       onInit() {
+			this.oModel = new JSONModel({selectedDates:[]});
+			this.getView().setModel(this.oModel);
+
         var oComboBox = this.byId("yearComboBox");
 
         // 현재 연도 및 범위 설정
@@ -50,16 +53,15 @@ sap.ui.define(
             
             aResults.forEach(function(item) {
               if(item.PoSDate && item.PoEDate) {
+                // 시작일 날짜 형식 변환
                 var date = new Date(item.PoSDate);
                 var year = date.getFullYear();
-                var month = (date.getMonth() + 1)
-                .toString()
-                .padStart(2, "0");
+                var month = (date.getMonth() + 1).toString().padStart(2, "0");
                 var day = date.getDate().toString().padStart(2, "0");
                 var formatDate = year + "-" + month + "-" + day;
                 item.PoSDate = formatDate;
 
-
+                // 종료일도 같은 방식으로 변환
                 var date1 = new Date(item.PoEDate);
                 var year1 = date1.getFullYear();
                 var month1 = (date.getMonth() + 1)
@@ -79,24 +81,40 @@ sap.ui.define(
             MessageToast.show("계획주문 데이터를 불러오지 못했습니다.");
             console.error(oError);
           },
-        });
-
-        var onSearch = this.byId("searchButton");
-        onSearch.attachPress(function () {
-          var selectedYear = oComboBox.getSelectedKey();
-          var selectedMonth = oMonthComboBox.getSelectedKey();
-
-          if (selectedYear && selectedMonth) {
-				aFilter.push(new Filter("YearAndMonth", FilterOperator.Contains, selectedYear + selectedMonth));
-          } else {
-            MessageToast.show("연도와 월을 선택하세요.");
-          }
-        }, this);
-
-
-
-
+        }); // 계획주문 테이블 불러오기
       },
+
+async openCalendar(oEvent) {
+  const oDetailBox = this.byId("detailBox"); // ✅ 이제 인식됨
+  oDetailBox.setVisible(true);
+
+  // 나머지 로직 추가 예정 (선택된 행 가져오기, 오더 확인 등)
+},
+
+		handleCalendarSelect(oEvent) {
+			var oCalendar = oEvent.getSource(),
+				aSelectedDates = oCalendar.getSelectedDates(),
+				oData = {
+					selectedDates: []
+				},
+				oDate,
+				i;
+			if (aSelectedDates.length > 0 ) {
+				for (i = 0; i < aSelectedDates.length; i++){
+					oDate = aSelectedDates[i].getStartDate();
+					oData.selectedDates.push({Date:this.oFormatYyyymmdd.format(oDate)});
+				}
+				this.oModel.setData(oData);
+			} else {
+				this._clearModel();
+			}
+		},
+
+    		handleRemoveSelection: function() {
+			this.byId("calendar").removeAllSelectedDates();
+			this._clearModel();
+		},
+
     });
   }
 );
