2bfbdecae3dd4b422d2630679acbbb545e9f8fa6
Tom-Jeong <jshipod98@gmail.com>
2025-06-18 18:25:39 +0900

    특강사항

---
+++ ws_cds_exam/webapp/controller/Main.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("sync.d20.wscdsexam.controller.Main", {
+        onInit() {
+        }
+    });
+});
---
+++ ws_cds_exam/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"syncd20/ws_cds_exam/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
